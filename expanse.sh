a2319fea534d3a5915f7ad3082f08c5530ae7268
MissileXCrisis <57077160+MissileXCrisis@users.noreply.github.com>
2023-12-11 12:38:47 -0800

    Update expanse.sh

--- expanse.sh
+++ expanse.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+OPTS="-Wall -O3"
+
+echo "This script builds the final project test program!"
+echo "By: Zoe Bozich, Alec Clark, and Missael Flores "
+g++ $OPTS -fopenmp -o potential_solver_parallel potential_solver_edits_parallel.cpp functions.h
+./potential_solver
+echo "Opening the plotting file..."
+python3 ./final_plotter.py

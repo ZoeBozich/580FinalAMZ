57e02bb0cbd3277337af75d8e055eba90e643f71
MissileXCrisis <57077160+MissileXCrisis@users.noreply.github.com>
2023-12-11 15:19:10 -0800

    Update expanse.sh

--- expanse.sh
+++ expanse.sh
@@ -13,17 +13,19 @@ R_x=5
 R_y=5
 rho_0=1
 grid_points=100  # Replace with required N
+num_threads=1    # Set the number of threads (HPC 1 thread per core) (laptop(model g14 2 threads per core)
+
 
 #This job runs with 1 nodes, 1 core per node and print hello world and date.
 #Environment
 module purge
-module load slurm
-module load cpu
-module load python/3.8.5
+module spider load slurm
+module spider load cpu
+module spider load python/3.8.5
 
 OPTS="-Wall -O3"
 
-g++ $OPTS -fopenmp -o potential_solver_parallel potential_solver_timer_parallel.cpp functions.h
+g++ $OPTS -fopenmp -o potential_solver_parallel hpc_potential_solver_timer_parallel.cpp functions.h
 ./potential_solver_parallel $L_x $L_y $R_x $R_y $rho_0 $grid_points
 
 echo "Opening the plotting file..."

aed7dbae014b1aacc2ae3629c83dd507f47031f0
MissileXCrisis <57077160+MissileXCrisis@users.noreply.github.com>
2023-12-13 10:36:19 -0800

    Update parallel_1_basic_potential_solver.cpp

--- parallel_1_basic_potential_solver.cpp
+++ parallel_1_basic_potential_solver.cpp
@@ -1,7 +1,7 @@
-#include "functions.h"
 #include <omp.h>
+#include <chrono>
 #include <iostream>
-#include <fstream>
+#include "functions.h"
 
 int main()
 {
@@ -19,13 +19,18 @@ int main()
     R[0] = fetch_bounded_double("R_x (m)", 0.5*L[0]);
     R[1] = fetch_bounded_double("R_y (m)", 0.5*L[1]);
 
+    Vec_DP r_0(2);
+    std::cout << "Please enter the x_0 and x_0 parameters, in meters:" << std::endl;
+    r_0[0] = fetch_bounded_double("x_0 (m)", L[0]);
+    r_0[1] = fetch_bounded_double("y_0 (m)", L[1]);
+
     // TODO - Check/fix units
     std::cout << "Please enter the rho_0 parameter in Coulombs per meter^5:" << std::endl;
     double rho_0 = fetch_pos_double("rho_0 (C*m^5)");
 
     // TODO - Require N to be divisible by 4 for Boole's array approach
     std::cout << "Please enter the number of grid points N for the Fourier integrals approach:" << std::endl;
-    int N = fetch_pos_int("N");
+    int N = fetch_pos_int("N") + 1;
 	
 	//Prompt number of threads for PARALLELIZATION
 	int num_threads;
@@ -34,58 +39,51 @@ int main()
 	//Set number of threads entered by the User
 	omp_set_num_threads(num_threads);
 
-
     // Creates a header to give relevant information to my python program
     // This is probably not a great approach to communicate with it, not sure how to improve!
     std::ofstream fp(DEFAULT_OUT);
     fp << KEY << std::endl;
-    fp << create_header(L, R, rho_0, N) << std::endl;
+    fp << create_header(L, R, r_0, rho_0, N) << std::endl;
     fp << KEY << std::endl;
 
     std::cout << "Calculating the potential..." << std::endl;
-
 	//TIMER BEGINS
-	double start_time = omp_get_wtime();
-
-    // Obtains the potential
+	double start_time = omp_get_wtime();	
+    
+	// Obtains the potential
+    double c_mn, rho_mn;
     Mat_DP V(N,N);
-    double c_mn, rho_mn, x, y;
-
+    Vec_DP x(N), y(N), h(2), args(2);
+    
+    h[0] = L[0]/(N-1);
+    h[1] = L[1]/(N-1);
+	
     for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < N; j++)
-        {
-            V[i][j] = 0.0;
-        }
+        x[i] = i * h[0];
+        y[i] = i * h[1];
     }
-
-    // Time for a messy block of code... This definitely needs fixing.
-    double h_x = L[0]/(N-1);
-    double h_y = L[1]/(N-1);
     
-    int temp = 1;//helping us skip the m&&n ==0 case to avoid dividing by zero.
-    
-	//PARALLELIZATION HERE 
+    //int temp = 1;//helping us skip the m&&n ==0 case to avoid dividing by zero.
+	
+	//PARALLELIZATION
 	#pragma omp parallel for collapse(2)
-	for (int m = 0; m < MAX_NM; m++){
-        for (int n = temp; n < MAX_NM; n++){//int n=temp
-			if (m == 0 && n == 0) continue; // Skip the m && n == 0 case
-				// A lot of redundancy here compared to fourier_double_int routine.
-				// Maybe can be merged into one big loop?
-				double arg_x = m * M_PI / L[0];
-				double arg_y = n * M_PI / L[1];
-				rho_mn = fourier_double_int(R, L, rho_0, N, arg_x, arg_y, h_x, h_y);// change function to accept arg_x, arg_y, hx, hy, don't need m, n
-				c_mn = rho_mn / (arg_x*arg_x + arg_y*arg_y);
-				for (int i = 0; i < N; i++)
-				{
-					x = i * h_x;
-					for (int j = 0; j < N; j++)
-					{
-						y = j * h_y;
-						// Note the 4/L_xL_y, to avoid using 2/sqrt(...) twice.
-						V[i][j] += 4.0*c_mn*cos(arg_x*x)*cos(arg_y*y)/(L[0]*L[1]);
+    for (int m = 0; m < N; m++){
+		for (int n = 0; n < N; n++) {
+			if (m == 0 && n == 0) continue; // Skip the case when both m and n are zero
+
+            // A lot of redundancy here compared to fourier_double_int routine.
+            // Maybe can be merged into one big loop?
+            args[0] = m * M_PI / L[0];
+            args[1] = n * M_PI / L[1];
+            rho_mn = fourier_double_int(R, L, r_0, rho_0, N, args, h, x, y);// change function to accept arg_x, arg_y, hx, hy, don't need m, n
+            c_mn = rho_mn / (args[0]*args[0] + args[1]*args[1]);
+            for (int i = 0; i < N; i++){
+                for (int j = 0; j < N; j++){
+                    // Note the 4/L_xL_y, to avoid using 2/sqrt(...) twice.
+                    V[i][j] += 4.0*c_mn*cos(args[0]*x[i])*cos(args[1]*y[j])/(L[0]*L[1]);
                 }
-            } 
+            }
         }
     }
 
@@ -94,25 +92,24 @@ int main()
     // Prints the results to file DEFAULT_OUT
     for (int i = 0; i < N; i++)
     {
-        x = i * h_x;
         for (int j = 0; j < N; j++)
         {
-            y = j * h_y;
-            fp << std::setw(W) << x;
-            fp << std::setw(W) << y;
-            fp << std::setw(W) << rho(x,y,R,L,rho_0);
+            fp << std::setw(W) << x[i];
+            fp << std::setw(W) << y[j];
+            fp << std::setw(W) << rho(x[i],y[j],R,L,r_0,rho_0);
             fp << std::setw(W) << V[i][j];
             fp << std::endl;
         }
     }
     std::cout << "The data file has been generated!" << std::endl;
-	
 	//TIMER END
     double end_time = omp_get_wtime();
 	//TIME CALCULATION
     double duration = end_time - start_time;
     //OUTPUT TIME
     std::cout << "Calculation completed in " << duration << " seconds." << std::endl;
-    
+	//Adding empty le=ine for readability
+    std::cout << '\n';
+	
 	return 0;
 }

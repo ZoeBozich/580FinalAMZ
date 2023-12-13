6751fe93db846d187bd75ef1b24bf58421eb514b
MissileXCrisis <57077160+MissileXCrisis@users.noreply.github.com>
2023-12-13 12:29:02 -0800

    Update parallel_3_HPC_python_plots.sh

--- parallel_3_HPC_python_plots.sh
+++ parallel_3_HPC_python_plots.sh
@@ -4,35 +4,28 @@
 #SBATCH --partition=shared
 #SBATCH --account=sds154
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 00:03:00
+#SBATCH --ntasks-per-node=64
+#SBATCH -t 00:10:00
 
 # Fixed values for dimensions, R_x, R_y, rho_0
-L_x=10
-L_y=10
-R_x=5
-R_y=5
+Lx=10
+Ly=10
+Rx=5
+Ry=5
 rho_0=1
 
-module  load slurm
-
-module load cpu/0.15.4 gcc/10.2.0
+# Array of N values and thread counts
+grids=(10 100 1000 10000 100000 1000000 10000000)
+threads=(64)
 
 
 # Compile the program
-echo "Compiling "
-g++ -Wall -O3 -fopenmp -o parallel_3 parallel_3_noninteractive_itterative.cpp functions.h
-chmod +x parallel_3
+g++ -Wall -O3 -fopenmp -o parallel_3 parallel_2_noninteractive_itterative_potential_solver.cpp functions.h
 
-
-threads=1
-grids=10
-echo "Computing"
-./parallel_3 $L_x $L_y $R_x $R_y $rho_0 $grids $threads
-
-threads=1
-grids=100
-echo "Computing"
-./parallel_3 $L_x $L_y $R_x $R_y $rho_0 $grids $threads
-
-echo "Done"
+# Run tests
+for n in "${grids[@]}"; do
+    for t in "${threads[@]}"; do
+        echo "Running for N=$n with $t threads..."
+        ./parallel_3  $Lx $Ly $Rx $Ry $rho_0 $n $t >> results.txt
+    done
+done

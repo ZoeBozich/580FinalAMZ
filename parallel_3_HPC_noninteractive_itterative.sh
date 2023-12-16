d9204700afbfc34017a5e41f5e592f417b72e05f
MissileXCrisis <57077160+MissileXCrisis@users.noreply.github.com>
2023-12-16 10:03:44 -0800

    Update parallel_3_HPC_noninteractive_itterative.sh

--- parallel_3_HPC_noninteractive_itterative.sh
+++ parallel_3_HPC_noninteractive_itterative.sh
@@ -3,9 +3,9 @@
 #SBATCH --output="potential_solver.%j.%N.out"
 #SBATCH --partition=shared
 #SBATCH --account=sds154
-#SBATCH --nodes=5
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 03:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH -t 03:30:00
 
 # Fixed values for dimensions, R_x, R_y, rho_0
 Lx=10
@@ -16,16 +16,16 @@ rho_0=1
 
 # Array of N values and thread counts
 grids=(10 100 1000 10000 100000 1000000 10000000)
-threads=(1 2 3 8 16 32 64 128)
+threads=(128 64 32 16 8 4 3 2 1)
 
 
 # Compile the program
 g++ -Wall -O3 -fopenmp -o parallel_3 parallel_2_noninteractive_itterative_potential_solver.cpp functions.h
 
 # Run tests
-for n in "${grids[@]}"; do
-    for t in "${threads[@]}"; do
-        echo "Running for N=$n with $t threads..."
-        ./parallel_2 $Lx $Ly $Rx $Ry $rho_0 $n $t >> results.txt
+for n in "${threads[@]}"; do
+    for t in "${grids[@]}"; do
+        echo "Running for $t threads with N=$n"
+        ./parallel_3 $Lx $Ly $Rx $Ry $rho_0 $n $t >> results.txt
     done
 done

f04a1052e42a9704043531981a57522d76428136
Alec Clark <alecclark28@gmail.com>
2023-12-22 15:13:35 -0800

    Delete extra directory

--- ExpanseCode/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "This script builds the final project on Expanse!"
-echo "By: Zoe Bozich, Alec Clark, and Missael Flores"
-
-OPTS="-Wall -O3 -fopenmp"
-
-# Can fill in the expanse script!
